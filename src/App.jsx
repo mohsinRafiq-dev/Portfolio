6d5ff96ee4424aba3d440e1fb14e8dcc33871272
mohsinRafiq.dev <muhammadmohsinrafiq54@gmail.com>
2025-03-12 23:42:48 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from '../Components/Navbar'
-import Footerr from '../Components/Footer/Footerr'
-import Home_page from '../Components/Pages/Home_page'
-import Projects_page from '../Components/Pages/Projects_page'
-import About_page from '../Components/Pages/About_page'
-import Contacts_page from '../Components/Pages/Contacts_page'
-import {BrowserRouter, Route,Routes} from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import "./App.css";
+import Navbar from "../Components/Navbar";
+import Footerr from "../Components/Footer/Footerr";
+import Home_page from "../Components/Pages/Home_page";
+import Projects_page from "../Components/Pages/Projects_page";
+import About_page from "../Components/Pages/About_page";
+import Contacts_page from "../Components/Pages/Contacts_page";
+
+const RouteChangeHandler = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    NProgress.start(); 
+    setTimeout(() => {
+      NProgress.done(); 
+    }, 50); 
+
+    return () => {
+      NProgress.done(); 
+    };
+  }, [location.path]); 
+
+  return null; 
+};
 
 function App() {
   return (
-    <>
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home_page/>}/>
-      <Route path='/projects' element={<Projects_page/>}/>
-      <Route path='/about-me' element={<About_page/>}/>
-      <Route path='/contacts' element={<Contacts_page/>}/>
-    </Routes>
-    <Footerr/>
+      <RouteChangeHandler />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home_page />} />
+        <Route path="/projects" element={<Projects_page />} />
+        <Route path="/about-me" element={<About_page />} />
+        <Route path="/contacts" element={<Contacts_page />} />
+      </Routes>
+      <Footerr />
     </BrowserRouter>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
